bc1dba177f1e4745b96e76b3997cd39e3e84ebfc
Scamir <gderamchi@gmail.com>
2026-03-01 04:18:13 +0100

    Revert "yo"
    This reverts commit 8268351f2a290ac1b21332cd08a2cca1cecf3b7b.

--- apps/api/src/modules/timeline/timeline.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-
-import { TimelineService } from "./timeline.service";
-
-@Module({
-  providers: [TimelineService],
-  exports: [TimelineService]
-})
-export class TimelineModule {}
